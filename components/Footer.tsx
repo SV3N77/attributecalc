0f095ae1454db08e1437f39682fd8547992d7020
SV3N77 <danny.jiang1234@hotmail.com>
2022-08-27 18:13:03 +1000

    update name and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer(): JSX.Element {
-  return <header className="flex bg-zinc-800 p-8"></header>;
+  return (
+    <footer className="flex justify-center bg-zinc-800 p-8">
+      <div className="text-white">Made with Next.js & TailwindCSS</div>
+    </footer>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <header className="flex bg-gradient-to-b from-zinc-800 to-zinc-700 p-8">
-      <div className="text-2xl font-bold text-white">Damage Calculator</div>
+      <div className="text-2xl font-bold text-white">
+        Elden Ring Attribute Calculator
+      </div>
     </header>
   );
 }
